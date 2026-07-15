c524d8ab26f4bac1f05123dfa9015e11dfe56821
Breadleaf <bkhnapa@gmail.com>
2026-07-15 14:59:33 -0700

    add calibration code and update readme with example run

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 These instructions are for slic3r forks. I have only tested on BambuStudio.
 
 1. Print `./PHC-test_jig.3mf` using your slicer's default settings
-2. Measure the holes with a caliper
+2. Measure the diameter of the holes with a caliper set to measure mm
 3. Run calibration script as follows:
 ```
 $(which python3) calibrate.py result1 result2 ... result5
@@ -12,3 +12,15 @@ Note: The order of results should be put in the order of smallest hole to
 largest from the expected results.
 4. Set the `Quality` -> `Precision` -> `X-Y hole/contour compensation` to the
 output of the script
+
+### Example Run:
+
+```
+python3 calibrate.py 9.86 10.01 10.14 10.17 10.26
+[*] expected 10.00 - result 9.86
+[*] expected 10.10 - result 10.01
+[*] expected 10.20 - result 10.14
+[*] expected 10.30 - result 10.17
+[*] expected 10.40 - result 10.26
+X-Y hole/contour compensation: 0.05600000000000005 (Radius)
+```
--- calibrate.py
+++ calibrate.py
@@ -1,1 +1,34 @@
-print("Hello World")
+import sys
+
+DEBUG = not False
+TARGETS = [ 10.0, 10.1, 10.2, 10.3, 10.4 ]
+
+if __name__ == "__main__":
+    data = sys.argv[1:]
+    if len(data) != len(TARGETS):
+        print(
+                f"[-] invalid result vector length: len({data}) = {len(data)}, expected: {len(TARGETS)}",
+                file=sys.stderr
+                )
+
+    converted_data = []
+    for item in data:
+        try:
+            converted_item = round(float(item), 2)
+            converted_data.append(converted_item)
+        except ValueError:
+            print(
+                    f"[-] item '{item}' cannot be converted to type float",
+                    file=sys.stderr
+                    )
+            sys.exit(1)
+
+    total_delta_diameter = 0
+    for result, target in zip(converted_data, TARGETS):
+        if DEBUG: print(f"[*] expected {target:.2f} - result {result:.2f}")
+        total_delta_diameter += target - result
+
+    average_delta_diameter = total_delta_diameter / len(converted_data)
+    average_radius_compensation = average_delta_diameter / 2
+
+    print(f"X-Y hole/contour compensation: {average_radius_compensation} (Radius)")
